5667f8cead792527db43a8a10922bf77e4186e9f
Gabriel Nunes <gnunesmail@gmail.com>
2022-06-07 13:40:35 -0300

    feat: criar component ClaimCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from '@components/_ui/Layout';
 import Page from '@components/_ui/Page';
 import Hero from '@components/home/Hero';
 import About from '@components/home/About';
+import ClaimCards from '@components/home/ClaimCards';
 
 export default function Conf() {
   return (
@@ -9,6 +10,7 @@ export default function Conf() {
       <Layout>
         <Hero />
         <About />
+        <ClaimCards />
       </Layout>
     </Page>
   );
