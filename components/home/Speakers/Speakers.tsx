34c233133c67f0173b29a590ef738489cba2d993
Gabriel Nunes <gnunesmail@gmail.com>
2022-06-07 20:38:58 -0300

    feat: seção speakers na home

--- components/home/Speakers/Speakers.tsx
+++ components/home/Speakers/Speakers.tsx
@@ -1,21 +1,49 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
 import { Grid, Column } from '@components/_ui/Grid';
 import LinkButton from '@components/_ui/LinkButton';
 
 import styles from './Speakers.module.scss';
 
-export default function Gather() {
+export default function Speakers() {
   return (
     <section>
       <Grid>
-        <Column lg={7}>
+        <Column lg={8}>
           <h2>São mais de 20 palestras e painéis com os principais nomes da área</h2>
         </Column>
       </Grid>
       <Grid>
-        <Column lg={3}>asdasdas</Column>
+        <Column lg={3} sm={4}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={4}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={4}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={4} xsm={0}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={4} xsm={0}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={4} xsm={0}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={0} xsm={0}>
+          [componente speaker]
+        </Column>
+        <Column lg={3} sm={0} xsm={0}>
+          [componente speaker]
+        </Column>
+      </Grid>
+
+      <Grid>
+        <Column lg={12}>
+          <div className={styles.cta}>
+            <LinkButton href="/programacao">Confira a programação completa</LinkButton>
+          </div>
+        </Column>
       </Grid>
     </section>
   );
