f10616a347f33c64a0cab2f31eec7263152fe1f1
Gabriel Nunes <gnunesmail@gmail.com>
2022-06-22 21:00:17 -0300

    feat: página de contato

--- components/contato/Contact/Contact.tsx
+++ components/contato/Contact/Contact.tsx
@@ -6,48 +6,45 @@ import IconDiscord from '@components/_ui/Icons/icon-discord';
 import LinkButton from '@components/_ui/LinkButton/LinkButton';
 
 import styles from './Contact.module.scss';
+import { Grid, Column } from '@components/_ui/Grid';
 
 export default function Contact() {
   return (
-    <>
-      <Header
-        image="/images/contato/hero.svg"
-        title="Entre em contato"
-        description="Tá com dúvida? Vem com a gente."
-      />
-      <div className={styles.container}>
-        <div className={styles.contact}>
-          <p>Dúvidas gerais</p>
-          <a href="mailto:sac@codecon.dev">sac@codecon.dev</a>
-        </div>
-        <div className={styles.contact}>
-          <p>Quer ser um patrocinador?</p>
+    <section>
+      <Grid>
+        <Column lg={3} sm={6}>
+          <h3>Quer ser nosso patrocinador?</h3>
           <a href="mailto:patrocinadores@codecon.dev">patrocinadores@codecon.dev</a>
-        </div>
-        <div className={styles.contact}>
-          <p>Para outros assuntos</p>
+        </Column>
+        <Column lg={3} sm={6}>
+          <h3>Está com alguma dúvida geral?</h3>
           <a href="mailto:contato@codecon.dev">contato@codecon.dev</a>
-        </div>
-      </div>
-      <div className={styles.social}>
-        <span>Também estamos nas redes sociais</span>
-
-        <LinkButton newPage href="https://twitter.com/codecondev">
-          <IconTwitter size={16} color="#fff" /> Twitter
-        </LinkButton>
-
-        <LinkButton newPage href="https://instagram.com/codecon.dev">
-          <InstagramIcon size={16} color="#fff" /> Instagram
-        </LinkButton>
-
-        <LinkButton newPage href="https://github.com/codecon-dev">
-          <IconGithub size={16} color="#fff" /> GitHub
-        </LinkButton>
-
-        <LinkButton newPage href="https://codecon.dev/discord">
-          <IconDiscord size={16} color="#fff" /> Discord
-        </LinkButton>
-      </div>
-    </>
+        </Column>
+        <Column lg={3} sm={6}>
+          <h3>Prefere falar por Whatsapp?</h3>
+          Manda no{' '}
+          <a href="https://wa.me/5547999159929" target="_blank" rel="noreferrer noopener">
+            (47) 99915-9929
+          </a>
+        </Column>
+        <Column lg={3} sm={6}>
+          <h3>Ou entre em contato pelas redes sociais</h3>
+          <div className={styles.social}>
+            <a href="https://twitter.com/codecondev" target="_blank" rel="noreferrer noopener">
+              <IconTwitter size={24} color="var(--light-green)" />
+            </a>
+            <a href="https://instagram.com/codecon.dev" target="_blank" rel="noreferrer noopener">
+              <InstagramIcon size={24} color="var(--light-green)" />
+            </a>
+            <a href="https://github.com/codecon-dev" target="_blank" rel="noreferrer noopener">
+              <IconGithub size={24} color="var(--light-green)" />
+            </a>
+            <a href="https://codecon.dev/discord" target="_blank" rel="noreferrer noopener">
+              <IconDiscord size={24} color="var(--light-green)" />
+            </a>
+          </div>
+        </Column>
+      </Grid>
+    </section>
   );
 }
--- pages/contato.tsx
+++ pages/contato.tsx
@@ -1,20 +1,42 @@
+import { GetStaticProps } from 'next';
+
+import { getAllSponsors } from '@lib/cms-api';
+import { Sponsor } from '@lib/types/all';
+
 import Page from '@components/_ui/Page';
 import Layout from '@components/_ui/Layout';
+import Header from '@components/_ui/Header';
 import Contact from '@components/contato/Contact';
 
-import { META_DESCRIPTION } from '@lib/constants';
+type Props = {
+  sponsors: Sponsor[];
+};
 
-export default function Palestras() {
+export default function Contato({ sponsors }: Props) {
   const meta = {
-    title: 'Contato - Codecon',
-    description: META_DESCRIPTION
+    title: 'Contato - Codecon'
   };
 
   return (
     <Page meta={meta}>
-      <Layout>
+      <Layout sponsors={sponsors}>
+        <Header
+          title="Entre em contato"
+          description="Quer ser nosso patrocinador? Está com alguma dúvida?"
+          image="/images/contato/hero.svg"
+        />
         <Contact />
       </Layout>
     </Page>
   );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const sponsors = await getAllSponsors();
+
+  return {
+    props: {
+      sponsors
+    }
+  };
+};
