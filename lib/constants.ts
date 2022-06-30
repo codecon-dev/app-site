4c58547c8ba35ca2b2f5920d8eb7b2aba5e68d82
Gabriel Nunes <gnunesmail@gmail.com>
2022-06-29 21:41:16 -0300

    fix: ajustar META_DESCRIPTION

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@ export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_U
 export const TWITTER_USER_NAME = 'codecondev';
 export const SITE_NAME = 'Codecon • 22, 23 e 24 de setembro';
 export const META_DESCRIPTION =
-  'Acompanhe os meetups semanais da nossa comunidade sobre desenvolvimento de software, ferramentas e tudo que há de bom.';
+  'A Codecon reúne código, diversão e atividades em um ambiente virtual onde você consegue de verdade interagir com outras pessoas.';
 export const REGISTER_URL = 'https://hotmart.com/checkout/918058';
 export const EVENT_PRICE = 9;
 export const WORKSHOP_PRICE = 30;
