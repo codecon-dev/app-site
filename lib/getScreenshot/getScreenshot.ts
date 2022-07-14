965f9ca62fb050b0269d4d59a4ea4e1a6e6ba521
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-07-14 16:59:55 -0300

    refactor: change return

--- lib/getScreenshot/getScreenshot.ts
+++ lib/getScreenshot/getScreenshot.ts
@@ -28,10 +28,7 @@ export default async function getScreenshot({ url = '', width = 2000, height = 1
     await page.setViewport({ width, height });
 
     await page.goto(url, { waitUntil: 'networkidle0' });
-    const screenshot = await page.screenshot({ type: 'png' });
-
-    await browser.close();
-    return screenshot;
+    return await page.screenshot({ type: 'png' });
   } catch (error) {
     console.error(error);
   }
