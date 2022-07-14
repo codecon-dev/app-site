a89b03cf4645781035b41e8b7d1515670a752435
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-07-14 16:55:26 -0300

    fix: puppeteer version

--- lib/getScreenshot/puppeteer.ts
+++ lib/getScreenshot/puppeteer.ts
@@ -1,4 +1,4 @@
 import puppeteerProd from 'puppeteer-core';
 import puppeteerDev from 'puppeteer';
 
-export default process.env.VERCEL_ENV === 'production' ? puppeteerProd : puppeteerDev;
+export default process.env.VERCEL_ENV === 'development' ? puppeteerDev : puppeteerProd;
