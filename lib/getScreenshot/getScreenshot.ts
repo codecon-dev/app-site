eb7e3cbdaf964329528e3bd87bb85e2c89e6f2d6
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-07-15 08:15:36 -0300

    refactor: getScreenshot

--- lib/getScreenshot/getScreenshot.ts
+++ lib/getScreenshot/getScreenshot.ts
@@ -1,26 +1,39 @@
 import chrome from 'chrome-aws-lambda';
-import puppeteer from './puppeteer';
+import puppeteer from 'puppeteer';
 
 export default async function getScreenshot({ url = '', width = 2000, height = 1000 } = {}) {
-  try {
-    const options =
-      process.env.VERCEL_ENV === 'development'
-        ? {
-            args: [],
-            executablePath:
-              process.platform === 'win32'
-                ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-                : process.platform === 'linux'
-                ? '/usr/bin/google-chrome'
-                : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-          }
-        : {
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless
-          };
+  let browser;
 
-    const browser = await puppeteer.launch(options);
+  async function getBrowserInstance() {
+    const executablePath = await chrome.executablePath;
+
+    if (!executablePath) {
+      // running locally
+      return puppeteer.launch({
+        args: chrome.args,
+        headless: true,
+        defaultViewport: {
+          width,
+          height
+        },
+        ignoreHTTPSErrors: true
+      });
+    }
+
+    return chrome.puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: {
+        width,
+        height
+      },
+      executablePath,
+      headless: chrome.headless,
+      ignoreHTTPSErrors: true
+    });
+  }
+
+  try {
+    browser = await getBrowserInstance();
     const page = await browser.newPage();
     await page.setUserAgent(
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:86.0) Gecko/20100101 Firefox/86.0'
@@ -28,8 +41,17 @@ export default async function getScreenshot({ url = '', width = 2000, height = 1
     await page.setViewport({ width, height });
 
     await page.goto(url, { waitUntil: 'networkidle0' });
-    return await page.screenshot({ type: 'png' });
+
+    const screenshot = await page.screenshot({
+      type: 'png'
+    });
+
+    return screenshot;
   } catch (error) {
     console.error(error);
+  } finally {
+    if (browser != null) {
+      await browser.close();
+    }
   }
 }
