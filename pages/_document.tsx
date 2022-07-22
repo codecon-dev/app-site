b02de89e6a638c2bca9c6c0fb045743e7b55de0d
Gabriel Nunes <gnunesmail@gmail.com>
2022-07-22 13:40:52 -0300

    fix: remover tag manager que não é usado

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default class CustomDocument extends Document {
-  renderGTMSnippet() {
-    return (
-      <>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VMX8ZXD3J0" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-VMX8ZXD3J0');
-        `
-          }}
-        />
-      </>
-    );
-  }
-
   render() {
     return (
       <Html lang="pt-BR">
-        <Head>{this.renderGTMSnippet()}</Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
