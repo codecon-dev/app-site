dd52d48bc70d4b27c63b32b775009e174331cfff
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-08-19 08:46:32 -0300

    fix: erro de mysql2 com sequelize

--- src/database/DataSource.ts
+++ src/database/DataSource.ts
@@ -1,20 +1,22 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const dataSource: Sequelize = new Sequelize(
-    process.env.DATABASE_NAME as string,
-    process.env.DATABASE_USER as string,
-    process.env.DATABASE_PASS as string,
-    {
-        host: process.env.DATABASE_HOST as string,
-        dialect: "mysql",
-        define: {
-            freezeTableName: true,
-            timestamps: true,
-            underscored: true
-        }
+  process.env.DATABASE_NAME as string,
+  process.env.DATABASE_USER as string,
+  process.env.DATABASE_PASS as string,
+  {
+    host: process.env.DATABASE_HOST as string,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    define: {
+      freezeTableName: true,
+      timestamps: true,
+      underscored: true
     }
-)
+  }
+);
 
-dataSource.sync({ alter: true })
+dataSource.sync({ alter: true });
 
-export default dataSource
+export default dataSource;
