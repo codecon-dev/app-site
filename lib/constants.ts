adff8d3186f427462dfc0dbab78b60316f107ed6
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-08-19 09:01:10 -0300

    fix: ajustar valor do ingresso

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const META_DESCRIPTION =
   'A Codecon reúne código, diversão e atividades em um ambiente virtual onde você consegue de verdade interagir com outras pessoas.';
 export const REGISTER_URL =
   'https://www.sympla.com.br/codecon-2022---be-the-developer-of-the-future__1631778';
-export const EVENT_PRICE = 39;
+export const EVENT_PRICE = 49;
 export const WORKSHOP_PRICE = 0;
 
 export const NAVIGATION = [
