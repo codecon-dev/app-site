63a47bd1ad9e024dfe75f884e25ce0f53d8195f1
Galeno de Melo <galenodemelo@gmail.com>
2022-08-19 17:48:16 -0300

    feat: Se o usuário acertou, salva no banco

--- src/services/PuzzleAnswerService.ts
+++ src/services/PuzzleAnswerService.ts
@@ -15,9 +15,21 @@ export default class PuzzleAnswerService {
         if (answeredAlready) return { success: true, message: "Enigma já respondido" }
 
         const normalizedUserGuess: string = this.normalize(userGuess)
+        const guessedCorrectly: boolean = normalizedUserGuess == this.normalize(puzzle.answer)
+        if (guessedCorrectly) {
+            await this.save(user, puzzle, PuzzleAnswerType.DONE)
+            return { success: true, message: "Acertou, mizerávi" }
+        }
         return { success: false, message: "Hmmm, não é isso" }
     }
 
+    private static async save(user: User, puzzle: Puzzle, status: PuzzleAnswerType = PuzzleAnswerType.PENDING): Promise<void> {
+        const [puzzleAnswer, created] = await PuzzleAnswer.findOrCreate({ where: { userId: user.id, puzzleId: puzzle.id } })
+
+        puzzleAnswer.status = status
+        puzzleAnswer.attempts++
+        await puzzleAnswer.save()
+    }
 
     private static normalize(value: string): string {
         return value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().trim()
