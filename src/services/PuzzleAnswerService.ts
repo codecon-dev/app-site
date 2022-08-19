a8eb201bfc7f05b89b54af86d6c92d4995814d71
Galeno de Melo <galenodemelo@gmail.com>
2022-08-19 17:46:08 -0300

    feat: Valida o enigma e se o usuário já respondeu

--- src/services/PuzzleAnswerService.ts
+++ src/services/PuzzleAnswerService.ts
@@ -8,6 +8,12 @@ export type PuzzleAnswerAttemptResponse = {
 export default class PuzzleAnswerService {
 
     public static async attempt(user: User, userGuess: string, puzzlePublicId: string): Promise<PuzzleAnswerAttemptResponse> {
+        const puzzle: Puzzle | null = await Puzzle.findByPublicId(puzzlePublicId)
+        if (!puzzle) throw new ResourceNotFoundError("Enigma não encontrado")
+
+        const answeredAlready: boolean = !!(await PuzzleAnswer.findOne({ where: { userId: user.id, puzzleId: puzzle.id, status: PuzzleAnswerType.DONE } }))
+        if (answeredAlready) return { success: true, message: "Enigma já respondido" }
+
         return { success: false, message: "Hmmm, não é isso" }
     }
 
