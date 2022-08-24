1b977697328f1a535471ab39d665d83b241e27ba
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-08-24 15:20:53 -0300

    fix: adicionar mensagem de loading no form e esconder depois de acerta

--- components/enigmas/Form/Form.tsx
+++ components/enigmas/Form/Form.tsx
@@ -5,61 +5,79 @@ import OneInputForm from '@components/_ui/OneInputForm';
 import { useUserData } from '@lib/hooks/useUserData';
 
 type Props = {
-  puzzlePublicId: string;
+    puzzlePublicId: string;
 };
 
 type ApiResponse = {
-  data: { success: boolean; message: string };
-  success: boolean;
-  message: string;
+    data: { success: boolean; message: string };
+    success: boolean;
+    message: string;
 };
 
 export default function Form({ puzzlePublicId }: Props) {
-  const [guess, setGuess] = useState('');
-  const { email } = useUserData();
+    const [guess, setGuess] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRight, setIsRight] = useState(false);
+    const { email } = useUserData();
 
-  async function handleSubmit(e: SyntheticEvent): Promise<void> {
-    e.preventDefault();
+    async function handleSubmit(e: SyntheticEvent): Promise<void> {
+        e.preventDefault();
 
-    if (!email) {
-      toast.error('Ocorreu um erro inesperado!');
-      return;
+        if (!email) {
+            toast.error('Ocorreu um erro inesperado!');
+            return;
+        }
+
+        setIsLoading(true);
+
+        const response = await fetch(`/api/games/puzzle/answer`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, guess, puzzlePublicId })
+        });
+
+        const json = await response.json();
+
+        setIsLoading(false);
+        const { data, success, message }: ApiResponse = json;
+        if (!success) {
+            toast.error(message);
+            return;
+        }
+
+        if (!data.success) {
+            toast(message, { icon: '🤔' });
+            return;
+        }
+
+        toast.success(message);
+        setIsRight(true);
     }
 
-    const response = await fetch(`/api/games/puzzle/answer`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, guess, puzzlePublicId })
-    });
-
-    const json = await response.json();
-    const { data, success, message }: ApiResponse = json;
-    if (!success) {
-      toast.error(message);
-      return;
+    if (isRight) {
+        return (
+            <section>
+                <div className="container">
+                    <h3>✅ Boa! Você acertou.</h3>
+                </div>
+            </section>
+        );
     }
 
-    if (!data.success) {
-      toast(message, { icon: '🤔' });
-      return;
-    }
-
-    toast.success(message);
-  }
-
-  return (
-    <section>
-      <div className="container">
-        <OneInputForm
-          handleSubmit={handleSubmit}
-          handleInputChange={event => setGuess(event.target.value)}
-          placeholder="Seu chute"
-          buttonText="Tentar"
-          disableSubmit={!guess || !email}
-        />
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <div className="container">
+                <OneInputForm
+                    handleSubmit={handleSubmit}
+                    handleInputChange={event => setGuess(event.target.value)}
+                    isLoading={isLoading}
+                    placeholder="Seu chute"
+                    buttonText="Tentar"
+                    disableSubmit={!guess || !email}
+                />
+            </div>
+        </section>
+    );
 }
