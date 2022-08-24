b17b166734c88ec448d23f9550af098032ddde0f
Galeno de Melo <galenodemelo@gmail.com>
2022-08-23 21:07:58 -0300

    refactor: Simplificando processamento do código

--- pages/api/codecodes/claim.ts
+++ pages/api/codecodes/claim.ts
@@ -3,27 +3,22 @@ import ValidationError from "@lib/errors/ValidationError"
 import { CodecodesClaimPayload, CodecodesClaimResponse } from "@lib/types/codecodes"
 import { StatusCodes } from "http-status-codes"
 import { NextApiRequest, NextApiResponse } from "next"
-import ApiResponse, { HttpMethod } from "src/api/ApiResponse"
+import ApiResponse, { HttpMethod, WithLoggedUserRequest } from "src/api/ApiResponse"
+import User from "src/database/model/User"
+
+export interface CodecodesClaimRequest extends WithLoggedUserRequest {
+    code: string
+}
 
 export default async function CodeCodesClaimController(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        if (req.method != HttpMethod.POST) {
-            ApiResponse.build(res, StatusCodes.METHOD_NOT_ALLOWED, "Método não permitido")
-            return
-        }
+    ApiResponse.withCurrentUserAndErrorHandler(req, res, HttpMethod.POST, async (user: User) => {
+        const params: CodecodesClaimRequest = req.body
+        if (!params.email || !params.code) throw new ValidationError("Tão faltando alguns dados nessa requisição")
 
-        const params: CodecodesClaimPayload = req.body
-        const codecodesResponse: CodecodesClaimResponse = await claimCodecodesApiToken(params)
+        const codecodesClaimPayload: CodecodesClaimPayload = { ...params, name: user.name }
+        const codecodesResponse: CodecodesClaimResponse = await claimCodecodesApiToken(codecodesClaimPayload)
         
         const statusCode: number = codecodesResponse.status == "success" ? StatusCodes.OK : StatusCodes.UNPROCESSABLE_ENTITY
         ApiResponse.build(res, statusCode, codecodesResponse.message, codecodesResponse.data)
-    } catch (error) {
-        if (error instanceof ValidationError) {
-            ApiResponse.buildForValidationError(res, error, req.url)
-            return
-        }
-
-        console.error(req.url, "Erro desconhecido", error)
-        ApiResponse.build(res, StatusCodes.INTERNAL_SERVER_ERROR, "Ocorreu um erro inesperado")
-    }
+    })
 }
