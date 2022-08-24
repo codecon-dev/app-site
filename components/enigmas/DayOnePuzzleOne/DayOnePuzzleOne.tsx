1fa1e95e6c3229a2559b4c5c484fa9b3655b3bcc
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-08-24 10:40:30 -0300

    feat: criar template para um componente de exemplo

---
+++ components/enigmas/DayOnePuzzleOne/DayOnePuzzleOne.tsx
@@ -0,0 +1,14 @@
+import Form from '../Form';
+import styles from './DayOnePuzzleOne.module.scss';
+
+export default function DayOnePuzzleOne() {
+  // TO-DO: Cada enigma tem um layout diferente, por isso será criado um novo componente pra cada enigma onde será feito o html e css dele
+
+  return (
+    <section>
+      <div className="container">
+        <Form puzzlePublicId="15RBaRo9WE6LC8c" />
+      </div>
+    </section>
+  );
+}
