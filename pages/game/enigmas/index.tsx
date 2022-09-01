2c352a7b5057cdf4d4667231c7494061b6264eda
Galeno de Melo <galenodemelo@gmail.com>
2022-08-31 22:09:41 -0300

    feat: Montando ranking dos enigmas
    # Filtrando apenas os acertos (`DONE`)
    # Agrupado por usuário
    # Conta a quantidade de acertos
    # Pega a data do último acerto

--- pages/game/enigmas/index.tsx
+++ pages/game/enigmas/index.tsx
@@ -1,6 +1,9 @@
 import Header from '@components/_ui/Header';
 import Layout from '@components/_ui/Layout';
 import Page from '@components/_ui/Page';
+import { GetServerSideProps } from 'next';
+import dataSource from 'src/database/DataSource';
+import PuzzleAnswer, { PuzzleAnswerStatus } from 'src/database/model/puzzle/PuzzleAnswer';
 type Props = {
 };
 
@@ -18,3 +21,24 @@ export default function EnigmaRanking({ puzzleRank }: Props) {
     );
 }
 
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const unformattedRankingList: Array<any> = await PuzzleAnswer.findAll({
+        attributes: [
+            'userId',
+            [dataSource.fn('COUNT', dataSource.col('id')), 'correctGuessCount'],
+            [dataSource.fn('MAX', dataSource.col('done_at')), 'lastCorrectGuessDate']
+        ],
+        where: { status: PuzzleAnswerStatus.DONE },
+        group: 'userId',
+        order: [
+            [dataSource.col('correctGuessCount'), 'DESC'],
+            [dataSource.col('lastCorrectGuessDate'), 'ASC']
+        ]
+    });
+
+    return {
+        props: {
+            puzzleRank: formattedRankingList
+        }
+    };
+};
