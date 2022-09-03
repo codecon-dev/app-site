b6f9bc03c26921b700b3a52fe08b5fc7f942a01b
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-09-03 16:16:46 -0300

    refac: use puzzle layout and move bg to public

--- components/enigmas/DayOnePuzzleOne/DayOnePuzzleOne.tsx
+++ components/enigmas/DayOnePuzzleOne/DayOnePuzzleOne.tsx
@@ -1,20 +1,14 @@
-import Form from '../Form';
 import styles from './DayOnePuzzleOne.module.scss';
-import NextImage from 'next/image';
-import bg from './bg.png'
+import PuzzleLayout from '../PuzzleLayout';
 
 export default function DayOnePuzzleOne() {
   return (
-    <section>
-      <div className={styles.bg}>
-        <NextImage src={bg} layout="fill"/>
+    <PuzzleLayout bgStyle={styles.bg}>
+      <div className={styles.text}>
+        <p>
+          Eu imagino que deva estar se sentindo um pouco como Alice, escorregando pela toca do coelho. Vejo isso em seus olhos...
+        </p>
       </div>
-      <p className={styles.text}>
-        Eu imagino que deva estar se sentindo um pouco como Alice, escorregando pela toca do coelho. Vejo isso em seus olhos...
-      </p>
-      <div className={styles.formContainer}>
-        <Form />
-      </div>
-    </section>
+    </PuzzleLayout>
   );
 }
