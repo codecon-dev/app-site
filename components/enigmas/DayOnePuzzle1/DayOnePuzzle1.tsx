952afcad6d0a72ca1c9332fc1eb54eaa0be70a96
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-09-05 11:27:43 -0300

    refac: move generic styles and remove div

--- components/enigmas/DayOnePuzzle1/DayOnePuzzle1.tsx
+++ components/enigmas/DayOnePuzzle1/DayOnePuzzle1.tsx
@@ -4,11 +4,9 @@ import PuzzleLayout from '../PuzzleLayout';
 export default function DayOnePuzzle1() {
   return (
     <PuzzleLayout bgStyle={styles.bg}>
-      <div className={styles.text}>
-        <p>
-          Eu i&nbsp;&nbsp;agino que deva est&nbsp;&nbsp;r se sen&nbsp;&nbsp;indo um pouco como Alice, esco&nbsp;&nbsp;regando pela toca do coelho. Vejo &nbsp;&nbsp;sso em seus olhos...
-        </p>
-      </div>
+      <p className={styles.text}>
+        Eu i&nbsp;&nbsp;agino que deva est&nbsp;&nbsp;r se sen&nbsp;&nbsp;indo um pouco como Alice, esco&nbsp;&nbsp;regando pela toca do coelho. Vejo &nbsp;&nbsp;sso em seus olhos...
+      </p>
     </PuzzleLayout>
   );
 }
