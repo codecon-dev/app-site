593c05b6311aa7e0e3f0d2c83f03853428695579
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-09-05 15:10:14 -0300

    feat: enigma 28

---
+++ components/enigmas/DayThreePuzzle28/DayThreePuzzle28.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Image from 'next/image';
+
+import PuzzleLayout from '../PuzzleLayout';
+import styles from './DayThreePuzzle28.module.scss';
+
+export default function DayOnePuzzle30() {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <PuzzleLayout bgStyle={styles.bg}>
+            {showModal && (
+                <div onMouseOut={() => setShowModal(false)} className={styles.modal}>
+                    <Image
+                        src="/images/enigmas/28-2.png"
+                        width={1208}
+                        height={441}
+                        layout="responsive"
+                    />
+                </div>
+            )}
+            <button className={styles.button} onMouseOver={() => setShowModal(true)} />
+        </PuzzleLayout>
+    );
+}
