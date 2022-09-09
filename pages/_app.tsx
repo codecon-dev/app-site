321e19ad417ae7db3499e68f76f5591e9d997841
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-09-09 16:58:02 -0300

    feat: add WhatsappFloatingButton to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,14 @@ import NProgress from '@components/_ui/Utils/NProgress';
 import ResizeHandler from '@components/_ui/Utils/ResizeHandler';
 
 import '@styles/base.scss';
+import WhatsappFloatingButton from '@components/_ui/WhatsappFloatingButton';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <OverlayProvider>
         <Component {...pageProps} />
+        <WhatsappFloatingButton />
         <ResizeHandler />
         <NProgress />
       </OverlayProvider>
