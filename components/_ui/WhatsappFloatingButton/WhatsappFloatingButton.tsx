365877bf99fe14e72ba9d8e74d4d7c5bc8f3574b
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-09-09 17:31:12 -0300

    refactor: change import name from whatsapp icon

--- components/_ui/WhatsappFloatingButton/WhatsappFloatingButton.tsx
+++ components/_ui/WhatsappFloatingButton/WhatsappFloatingButton.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 
 import styles from './WhatsappFloatingButton.module.scss';
 import { WHATSAPP_NUMBER } from '@lib/constants';
-import Whatsapp from '@components/_ui/Icons/icon-whatsapp';
+import WhatsappIcon from '@components/_ui/Icons/icon-whatsapp';
 
 const getWhatsappLink = (phone: string) => `https://api.whatsapp.com/send?phone=${phone}`;
 
 export default function WhatsappFloatingButton({ phone = WHATSAPP_NUMBER }) {
-  const whatsappLink = getWhatsappLink(phone);
+    const whatsappLink = getWhatsappLink(phone);
 
-  return (
-    <Link href={whatsappLink}>
-      <a target="_blank" rel="noopener noreferrer" className={styles.whatsapp_button}>
-        <Whatsapp />
-      </a>
-    </Link>
-  );
+    return (
+        <Link href={whatsappLink}>
+            <a target="_blank" rel="noopener noreferrer" className={styles.whatsapp_button}>
+                <WhatsappIcon />
+            </a>
+        </Link>
+    );
 }
