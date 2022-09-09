8e732c3b82d4856f6380593e2cd8e3ac41efd6c5
kaiofelipejs <kaiofelipe.js@gmail.com>
2022-09-09 17:37:39 -0300

    fix: move WhatsappFloating to Layout with rules

--- components/_ui/Layout/Layout.tsx
+++ components/_ui/Layout/Layout.tsx
@@ -3,22 +3,24 @@ import cn from 'classnames';
 import { Sponsor } from '@lib/types/all';
 import Navbar from '@components/_ui/Navbar';
 import Footer from '@components/_ui/Footer';
+import WhatsappFloatingButton from '@components/_ui/WhatsappFloatingButton';
 
 import styles from './Layout.module.scss';
 
 type Props = {
-  children: React.ReactNode;
-  hideNav?: boolean;
-  hideFooter?: boolean;
-  sponsors?: Sponsor[];
+    children: React.ReactNode;
+    hideNav?: boolean;
+    hideFooter?: boolean;
+    sponsors?: Sponsor[];
 };
 
 export default function Layout({ children, hideNav, hideFooter, sponsors }: Props) {
-  return (
-    <>
-      {!hideNav && <Navbar />}
-      <main className={cn(styles.main)}>{children}</main>
-      {!hideFooter && <Footer sponsors={sponsors} />}
-    </>
-  );
+    return (
+        <>
+            {!hideNav && <Navbar />}
+            <main className={cn(styles.main)}>{children}</main>
+            {(!hideNav || !hideFooter) && <WhatsappFloatingButton />}
+            {!hideFooter && <Footer sponsors={sponsors} />}
+        </>
+    );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,15 @@ import NProgress from '@components/_ui/Utils/NProgress';
 import ResizeHandler from '@components/_ui/Utils/ResizeHandler';
 
 import '@styles/base.scss';
-import WhatsappFloatingButton from '@components/_ui/WhatsappFloatingButton';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SSRProvider>
-      <OverlayProvider>
-        <Component {...pageProps} />
-        <WhatsappFloatingButton />
-        <ResizeHandler />
-        <NProgress />
-      </OverlayProvider>
-    </SSRProvider>
-  );
+    return (
+        <SSRProvider>
+            <OverlayProvider>
+                <Component {...pageProps} />
+                <ResizeHandler />
+                <NProgress />
+            </OverlayProvider>
+        </SSRProvider>
+    );
 }
