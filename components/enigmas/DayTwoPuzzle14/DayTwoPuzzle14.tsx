7cc3119150ee501e18498956c94b7d3b7ec21a4a
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-10 17:23:07 -0300

    feat: enigma 14

---
+++ components/enigmas/DayTwoPuzzle14/DayTwoPuzzle14.tsx
@@ -0,0 +1,6 @@
+import PuzzleLayout from '../PuzzleLayout';
+import styles from './DayTwoPuzzle14.module.scss';
+
+export default function DayTwoPuzzle14() {
+    return <PuzzleLayout bgStyle={styles.bg} />;
+}
