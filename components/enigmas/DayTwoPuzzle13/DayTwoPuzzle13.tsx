ad3c9f92a305017c8ba757d18d65b8df71263726
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-10 17:26:59 -0300

    geat: enigma 13

---
+++ components/enigmas/DayTwoPuzzle13/DayTwoPuzzle13.tsx
@@ -0,0 +1,6 @@
+import PuzzleLayout from '../PuzzleLayout';
+import styles from './DayTwoPuzzle13.module.scss';
+
+export default function DayTwoPuzzle13() {
+    return <PuzzleLayout bgStyle={styles.bg} />;
+}
