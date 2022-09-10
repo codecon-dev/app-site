e354da70f4dcf1ae7fcccc3031d84ed4da186c05
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-10 15:00:43 -0300

    feat: enigma 16

---
+++ components/enigmas/DayTwoPuzzle16/DayTwoPuzzle16.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import PuzzleLayout from '../PuzzleLayout';
+
+export default function DayTwoPuzzle16() {
+    return (
+        <PuzzleLayout>
+            <Image src="/images/enigmas/16.png" width={320} height={138} quality={100} />
+        </PuzzleLayout>
+    );
+}
