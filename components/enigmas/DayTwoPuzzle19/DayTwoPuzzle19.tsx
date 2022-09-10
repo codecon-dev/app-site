268877535c69e3aea691dceaae594cb5ac9c539f
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-10 14:57:58 -0300

    fix: remover arquivo não usado

--- components/enigmas/DayTwoPuzzle19/DayTwoPuzzle19.tsx
+++ components/enigmas/DayTwoPuzzle19/DayTwoPuzzle19.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
 import PuzzleLayout from '../PuzzleLayout';
-import styles from './DayTwoPuzzle19.module.scss';
 
 export default function DayTwoPuzzle19() {
     return (
-        <PuzzleLayout bgStyle={styles.bg}>
+        <PuzzleLayout>
             <Image src="/images/enigmas/19.png" width={1440} height={805} />
         </PuzzleLayout>
     );
