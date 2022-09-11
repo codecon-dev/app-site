9ceb0c696ddcac2522ba7d1b11878f11b39f1ff6
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-11 08:28:22 -0300

    feat: enigma 3

---
+++ components/enigmas/DayOnePuzzle3/DayOnePuzzle3.tsx
@@ -0,0 +1,10 @@
+import PuzzleLayout from '../PuzzleLayout';
+import Image from 'next/image';
+
+export default function DayOnePuzzle3() {
+    return (
+        <PuzzleLayout>
+            <Image src="/images/enigmas/3.png" width={821} height={640} quality={100} />
+        </PuzzleLayout>
+    );
+}
