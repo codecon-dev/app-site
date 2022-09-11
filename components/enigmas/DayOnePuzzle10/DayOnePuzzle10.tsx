0a3276600451b56634f66da959901aaad0d273af
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-11 09:03:54 -0300

    feat: enigma 10

---
+++ components/enigmas/DayOnePuzzle10/DayOnePuzzle10.tsx
@@ -0,0 +1,6 @@
+import PuzzleLayout from '../PuzzleLayout';
+import styles from './DayOnePuzzle10.module.scss';
+
+export default function DayOnePuzzle10() {
+    return <PuzzleLayout bgStyle={styles.bg} />;
+}
