9534a19e81c71d712e44c912e8992b78c24f40e7
Gabriel Nunes <gnunesmail@gmail.com>
2022-09-11 08:30:27 -0300

    feat: enigma 4

---
+++ components/enigmas/DayOnePuzzle4/DayOnePuzzle4.tsx
@@ -0,0 +1,10 @@
+import PuzzleLayout from '../PuzzleLayout';
+import Image from 'next/image';
+
+export default function DayOnePuzzle4() {
+    return (
+        <PuzzleLayout>
+            <Image src="/images/enigmas/4.png" width={923} height={474} quality={100} />
+        </PuzzleLayout>
+    );
+}
