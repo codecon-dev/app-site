d333be85c5f3c42e69c037426f75f1d0de8b404b
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-09-21 18:06:17 -0300

    feat: encerrar inscrições

--- components/home/Hero/Hero.tsx
+++ components/home/Hero/Hero.tsx
@@ -10,40 +10,40 @@ import { EVENT_PRICE, REGISTER_URL } from '@lib/constants';
 import styles from './Hero.module.scss';
 
 export default function Hero() {
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <header className={styles.header}>
-      <Grid align="center">
-        <Column lg={6} sm={7} smOrder={1}>
-          <h1>Be the developer of the future</h1>
-          <span className="headline headline__darker">
-            Online <span className="bullet">•</span> 22, 23 e 24 de Setembro
-          </span>
+    return (
+        <header className={styles.header}>
+            <Grid align="center">
+                <Column lg={6} sm={7} smOrder={1}>
+                    <h1>Be the developer of the future</h1>
+                    <span className="headline headline__darker">
+                        Online <span className="bullet">•</span> 22, 23 e 24 de Setembro
+                    </span>
 
-          <span className={styles['button__wrapper']}>
-            <LinkButton info={EVENT_PRICE} href={REGISTER_URL} newPage>
-              Inscreva-se
-            </LinkButton>
-          </span>
+                    <span className={styles['button__wrapper']}>
+                        <LinkButton href={REGISTER_URL} newPage>
+                            Inscrições encerradas
+                        </LinkButton>
+                    </span>
 
-          <DarkModeToggle />
-        </Column>
-        <Column lg={6} sm={5} xsmOrder={1}>
-          <span className={cn(styles['image__wrapper'], { [styles.loading]: isLoading })}>
-            <Player
-              autoplay
-              loop
-              onEvent={event => {
-                if (event === 'load') setIsLoading(false);
-              }}
-              src="/animations/hero.json"
-            />
-            <span className={styles.detail} />
-            <span className={styles['detail-two']} />
-          </span>
-        </Column>
-      </Grid>
-    </header>
-  );
+                    <DarkModeToggle />
+                </Column>
+                <Column lg={6} sm={5} xsmOrder={1}>
+                    <span className={cn(styles['image__wrapper'], { [styles.loading]: isLoading })}>
+                        <Player
+                            autoplay
+                            loop
+                            onEvent={event => {
+                                if (event === 'load') setIsLoading(false);
+                            }}
+                            src="/animations/hero.json"
+                        />
+                        <span className={styles.detail} />
+                        <span className={styles['detail-two']} />
+                    </span>
+                </Column>
+            </Grid>
+        </header>
+    );
 }
