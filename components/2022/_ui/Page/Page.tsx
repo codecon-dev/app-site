d198a3be1c46df714cb675388da8b4ca515dcbd4
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-12-23 10:37:47 -0300

    feat: corrigir imagens

--- components/2022/_ui/Page/Page.tsx
+++ components/2022/_ui/Page/Page.tsx
@@ -6,49 +6,49 @@ import { useRouter } from 'next/router';
 import { META_DESCRIPTION, SITE_NAME, SITE_URL, TWITTER_USER_NAME } from '@lib/constants';
 
 type Meta = {
-  title: string;
-  description?: string;
-  image?: string;
-  url?: string;
+    title: string;
+    description?: string;
+    image?: string;
+    url?: string;
 };
 
 type Props = {
-  meta?: Meta;
-  children: React.ReactNode;
-  fullViewport?: boolean;
+    meta?: Meta;
+    children: React.ReactNode;
+    fullViewport?: boolean;
 };
 
 export default function Page({ meta, children, fullViewport = false }: Props) {
-  const router = useRouter();
-  const image = meta?.image || '/share-image.png';
-  const title = meta?.title || SITE_NAME;
-  const url = meta?.url || `${SITE_URL}${router.asPath}`;
-  const description = meta?.description || META_DESCRIPTION;
+    const router = useRouter();
+    const image = meta?.image || '/images/2022/share-image.png';
+    const title = meta?.title || SITE_NAME;
+    const url = meta?.url || `${SITE_URL}${router.asPath}`;
+    const description = meta?.description || META_DESCRIPTION;
 
-  return (
-    <div className={cn('page-container', { full: fullViewport })}>
-      <Head>
-        <title>{title}</title>
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content={url} />
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
-        <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        {image && (
-          <meta
-            property="og:image"
-            content={image.startsWith('https://') ? image : `${SITE_URL}${image}`}
-          />
-        )}
-      </Head>
-      <Toaster />
-      {children}
-    </div>
-  );
+    return (
+        <div className={cn('page-container', { full: fullViewport })}>
+            <Head>
+                <title>{title}</title>
+                <meta property="og:title" content={title} />
+                <meta property="og:url" content={url} />
+                <meta name="description" content={description} />
+                <meta property="og:description" content={description} />
+                <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
+                <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                {image && (
+                    <meta
+                        property="og:image"
+                        content={image.startsWith('https://') ? image : `${SITE_URL}${image}`}
+                    />
+                )}
+            </Head>
+            <Toaster />
+            {children}
+        </div>
+    );
 }
