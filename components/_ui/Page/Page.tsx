9adf618b411d9aa1a87781d4050b7e23e89a47e9
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2023-03-17 16:42:24 -0300

    fix: ajustar imagens

--- components/_ui/Page/Page.tsx
+++ components/_ui/Page/Page.tsx
@@ -70,10 +70,8 @@ export default function Page({
                     <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
                     <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="icon" type="image/png" href="/favicon-32x32.png" />
                     <link rel="manifest" href="/site.webmanifest" />
-                    <link rel="shortcut icon" href="/favicon.ico" />
                     {image && (
                         <meta
                             property="og:image"
