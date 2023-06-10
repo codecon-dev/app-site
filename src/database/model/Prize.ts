b22c6d45c50e2c9a44dd21273cfd556c2b02bf99
Galeno de Melo <galenodemelo@gmail.com>
2023-06-09 23:05:28 -0300

    feat: Quantidade de prêmios sorteáveis refactor: Não salva usuário que deu redeem

--- src/database/model/Prize.ts
+++ src/database/model/Prize.ts
@@ -1,7 +1,6 @@
-import { DataTypes, ForeignKey } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import dataSource from '../DataSource';
 import ModelImpl, { commonAttributes } from './ModelImpl';
-import User from './User';
 
 export enum PrizeType {
     CODE_CODES = 'CODE_CODES',
@@ -10,26 +9,20 @@ export enum PrizeType {
 
 class Prize extends ModelImpl<Prize> {
     declare type: PrizeType;
-    declare redeemed: boolean;
-    declare redeemedAt: Date;
+    declare remaining?: number;
     declare description: string;
-
-    declare userId?: ForeignKey<User>;
 }
 
 Prize.init(
     {
         ...commonAttributes,
         type: { type: DataTypes.ENUM(...Object.keys(PrizeType)) },
-        redeemed: { type: DataTypes.BOOLEAN },
-        redeemedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+        remaining: { type: DataTypes.INTEGER, allowNull: true },
         description: { type: DataTypes.STRING }
     },
     {
         sequelize: dataSource
     }
 );
-
-Prize.belongsTo(User, { foreignKey: { allowNull: true }, as: 'user' });
 
 export default Prize;
