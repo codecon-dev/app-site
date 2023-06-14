b8309484c71c11520ea0be119c3d43eb25955a5a
Bruno Fuentes <brunofuentess@gmail.com>
2023-06-14 08:53:10 -0300

    feat: add opção de bg color via prop

--- components/enigmas/PuzzleLayout/PuzzleLayout.tsx
+++ components/enigmas/PuzzleLayout/PuzzleLayout.tsx
@@ -9,16 +9,20 @@ import { useRouter } from 'next/router';
 type Props = {
     bg?: string;
     bgContent?: string;
+    bgColor?: string;
     children?: ReactNode;
 };
 
-export default function PuzzleLayout({ bg, bgContent, children }: Props) {
+export default function PuzzleLayout({ bg, bgContent, children, bgColor }: Props) {
     const router = useRouter();
     const puzzlePublicId = getLastPath(router.pathname);
 
     return (
         <section className={cn(styles.layout)} style={{ backgroundImage: `url(${bg})` }}>
-            <div className={styles['content-bg']} style={{ backgroundImage: `url(${bgContent})` }}>
+            <div
+                className={styles['content-bg']}
+                style={{ backgroundImage: `url(${bgContent})`, backgroundColor: `${bgColor}` }}
+            >
                 <div className={cn(styles.content, 'container')}>{children}</div>
             </div>
             <div className={cn(styles.form, 'container')}>
