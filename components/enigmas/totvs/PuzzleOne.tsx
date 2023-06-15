3d922d7a9667815439ca14f099afa5abff94f52d
Bruno Fuentes <brunofuentess@gmail.com>
2023-06-14 21:30:44 -0300

    feat: add bg

--- components/enigmas/totvs/PuzzleOne.tsx
+++ components/enigmas/totvs/PuzzleOne.tsx
@@ -2,7 +2,7 @@ import PuzzleLayout from '../PuzzleLayout';
 
 export default function Puzzle() {
     return (
-        <PuzzleLayout>
+        <PuzzleLayout bgContent="/images/enigmas/totvs/11-bg.png">
             <img src="/images/enigmas/totvs/1.png" />
         </PuzzleLayout>
     );
