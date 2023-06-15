704192d7dea20f796ece431cef241daf963d249f
Bruno Fuentes <brunofuentess@gmail.com>
2023-06-14 21:13:50 -0300

    fix: mudar bg para bgContent

--- components/enigmas/totvs/PuzzleNineteen.tsx
+++ components/enigmas/totvs/PuzzleNineteen.tsx
@@ -2,7 +2,7 @@ import PuzzleLayout from '../PuzzleLayout';
 
 export default function Puzzle() {
     return (
-        <PuzzleLayout bg="/images/enigmas/totvs/11-bg.png">
+        <PuzzleLayout bgContent="/images/enigmas/totvs/11-bg.png">
             <img src="/images/enigmas/totvs/19.png" />
         </PuzzleLayout>
     );
