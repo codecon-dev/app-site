bb9d79b2de3415ce312fe1dd4656c0dc304da619
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2023-06-17 17:15:12 -0300

    feat: paginas dos enigmas

---
+++ pages/digital/game/enigmas/globo/4-BLC3ZhAXLqpc2C.tsx
@@ -0,0 +1,16 @@
+import Page from '@components/_ui/Page';
+import PrivateArea from '@components/_ui/PrivateArea';
+
+export default function PuzzleZero() {
+    const meta = {
+        title: 'Enigmas - Codecon Digital'
+    };
+
+    return (
+        <Page theme="digital" meta={meta} hideNav hideFooter noPadding>
+            <PrivateArea>
+                <></>
+            </PrivateArea>
+        </Page>
+    );
+}
