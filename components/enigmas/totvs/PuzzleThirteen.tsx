bb946a8ba99dbf8a9b9c8f3be31742647bd4e037
Bruno Fuentes <brunofuentess@gmail.com>
2023-06-20 20:15:17 -0300

    add PuzzleThirteen

--- components/enigmas/totvs/PuzzleThirteen.tsx
+++ components/enigmas/totvs/PuzzleThirteen.tsx
@@ -1,5 +1,12 @@
 import PuzzleLayout from '../PuzzleLayout';
 
 export default function Puzzle() {
-    return <PuzzleLayout bgColor="#8B949E"></PuzzleLayout>;
+    return (
+        <PuzzleLayout bgContent="/images/enigmas/totvs/11-bg.png">
+            <div style={{ display: 'flex', gap: '2rem' }}>
+                <img src="/images/enigmas/totvs/13-1.png" />
+                <img src="/images/enigmas/totvs/13-2.png" />
+            </div>
+        </PuzzleLayout>
+    );
 }
