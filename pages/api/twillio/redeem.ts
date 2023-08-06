13f9dff3426581401b6de274e14c2dad662876ce
Galeno de Melo <galenodemelo@gmail.com>
2023-08-06 18:04:55 -0300

    refactor: Removendo validação de formato de telefone
    # O formato a ser recebido pode variar demais

--- pages/api/twillio/redeem.ts
+++ pages/api/twillio/redeem.ts
@@ -1,11 +1,11 @@
-import { CodecodesClaimResponse } from "@lib/types/codecodes";
-import User from "@models/User";
-import { StatusCodes } from "http-status-codes";
-import { NextApiRequest, NextApiResponse } from "next";
-import FormatHelper from "src/FormatHelper";
-import ApiKeyValidator from "src/api/ApiKeyValidator";
-import ApiResponse from "src/api/ApiResponse";
-import CodeCodesService from "src/services/CodeCodesService";
+import { CodecodesClaimResponse } from '@lib/types/codecodes';
+import User from '@models/User';
+import { StatusCodes } from 'http-status-codes';
+import { NextApiRequest, NextApiResponse } from 'next';
+import FormatHelper from 'src/FormatHelper';
+import ApiKeyValidator from 'src/api/ApiKeyValidator';
+import ApiResponse from 'src/api/ApiResponse';
+import CodeCodesService from 'src/services/CodeCodesService';
 
 export default async function twillioRedeem(req: NextApiRequest, res: NextApiResponse) {
     try {
@@ -19,15 +19,6 @@ export default async function twillioRedeem(req: NextApiRequest, res: NextApiRes
         }
 
         const mobilePhone = FormatHelper.removeNonNumeric(req.body.mobilePhone as string);
-        if (!FormatHelper.validatePhone(mobilePhone)) {
-            ApiResponse.build(
-                res,
-                StatusCodes.UNPROCESSABLE_ENTITY,
-                `Parâmetro [mobilePhone] deve conter ${FormatHelper.MOBILE_PHONE_MIN_LENGTH} números, incluindo DDI e DDD. Valor recebido: [${mobilePhone}]`
-            );
-            return;
-        }
-
         const user = await User.findByMobilePhone(mobilePhone);
         if (!user) {
             ApiResponse.build(res, StatusCodes.UNPROCESSABLE_ENTITY, 'Usuário não encontrado');
@@ -36,15 +27,29 @@ export default async function twillioRedeem(req: NextApiRequest, res: NextApiRes
 
         const codeToRedeem: string = req.body.code;
         if (!codeToRedeem?.trim().length) {
-            ApiResponse.build(res, StatusCodes.UNPROCESSABLE_ENTITY, 'Obrigatório informar um código [code]');
+            ApiResponse.build(
+                res,
+                StatusCodes.UNPROCESSABLE_ENTITY,
+                'Obrigatório informar um código [code]'
+            );
             return;
         }
 
-        const codecodesResponse: CodecodesClaimResponse = await CodeCodesService.claimCode(user, codeToRedeem);
-        if (codecodesResponse.status !== "success") {
-            console.error("twillioRedeem >> Ocorreu um erro inesperado ao resgatar pontos", JSON.stringify(codecodesResponse));
-            ApiResponse.build(res, StatusCodes.INTERNAL_SERVER_ERROR, 'Ocorreu um erro ao resgatar os pontos');
-            return
+        const codecodesResponse: CodecodesClaimResponse = await CodeCodesService.claimCode(
+            user,
+            codeToRedeem
+        );
+        if (codecodesResponse.status !== 'success') {
+            console.error(
+                'twillioRedeem >> Ocorreu um erro inesperado ao resgatar pontos',
+                JSON.stringify(codecodesResponse)
+            );
+            ApiResponse.build(
+                res,
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                'Ocorreu um erro ao resgatar os pontos'
+            );
+            return;
         }
 
         ApiResponse.build(res, StatusCodes.OK, 'OK');
