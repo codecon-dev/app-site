381fc0fca6d4e1d5ab38110be2cfc4251a28ddfe
Gabriel Nunes <gnunesmail@gmail.com>
2024-06-27 10:14:47 -0300

    fix: origin para tudo

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 source: '/api/talks',
                 headers: [
                     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    { key: 'Access-Control-Allow-Origin', value: 'https://app.codecon.dev/' }, // replace this your actual origin
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
                     { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
                     {
                         key: 'Access-Control-Allow-Headers',
